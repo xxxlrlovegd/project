d301538ee05d4d295eb9b3fceebe7ecbf2a0f6e7
lu-ruixx <lurui@baai.ac.cn>
2025-01-07 16:29:21 +0800

    update:打包路径修改；

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
+  build: {
+    sourcemap: false,
+    minify: "terser",
+    chunkSizeWarningLimit: 1500,
+  },
 })
